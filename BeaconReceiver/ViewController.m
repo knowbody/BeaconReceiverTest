99ffd5aa97321e6a3507c31eef53c294ba2973cb
Mateusz Zatorski <mateuszzatorski@gmail.com>
2014-06-16 11:33:49 +0100

    initial commit

--- BeaconReceiver/ViewController.m
+++ BeaconReceiver/ViewController.m
@@ -18,6 +18,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    // Initialize location manager and set ourselves as the delegate
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    
+    // Create a NSUUID with the same UUID as the broadcasting beacon
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"A0ED1833-DF51-4E53-BD59-98AED83EEB70"];
+    
+    // Setup a new region with that UUID and same identifier as the broadcasting beacon
+    self.myBeaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
+                                                             identifier:@"com.knowbody.testregion"];
+    
+    // Tell location manager to start monitoring for the beacon region
+    [self.locationManager startMonitoringForRegion:self.myBeaconRegion];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +41,30 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+- (void)locationManager:(CLLocationManager*)manager didEnterRegion:(CLRegion*)region
+{
+    [self.locationManager startRangingBeaconsInRegion:self.myBeaconRegion];
+}
+
+-(void)locationManager:(CLLocationManager*)manager didExitRegion:(CLRegion*)region
+{
+    [self.locationManager stopRangingBeaconsInRegion:self.myBeaconRegion];
+    self.statusLabel.text = @"No";
+}
+
+-(void)locationManager:(CLLocationManager*)manager
+       didRangeBeacons:(NSArray*)beacons
+              inRegion:(CLBeaconRegion*)region
+{
+    // Beacon found!
+    self.statusLabel.text = @"Beacon found!";
+    
+}
+
+
 
 @end
